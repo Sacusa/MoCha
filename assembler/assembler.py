d475722eeb3b70b759674aac0838d37e0791a01f
Sudhanshu Gupta <Sacusa@users.noreply.github.com>
2018-03-09 19:31:14 +0530

    Update assembler
    The assembler is now fully functional. Verification, cleanup and documentation needed.

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -4,27 +4,38 @@ import os
 import re
 import sys
 
+# ordered tuple of all instructions
+ALL_INST = ('NOP', 'STP', 'MVD', 'MVS', 'MVI', 'MVB', 'STR', 'LOD', 'NOT', 'INC', 'DCR', 'ADA',
+            'SBA', 'XRA', 'ANA', 'ORA', 'ADI', 'SBI', 'XRI', 'ANI', 'ORI', 'JPD', 'JPP', 'JPR',
+            'CAD', 'CAR', 'RET', 'PSH', 'POP', 'RRC', 'RLC')
+
+# set of instructions that have immediate values
+IMM_VAL_INST = {'MVI', 'ADI', 'SBI', 'XRI', 'ANI', 'ORI', 'JPD', 'JPP', 'CAD'}
+
 # set of instructions that use flags
 FLAG_INST = {'JPD', 'JPP', 'JPR', 'CAD', 'CAR', 'RET'}
 
 # set of instructions that can use labels for addresses
 LABEL_ADDR_INST = {'JPD', 'JPP', 'CAD'}
 
-# set of instructions that have immediate values
-IMM_VAL_INST = {'MVI', 'ADI', 'SBI', 'XRI', 'ANI', 'ORI', 'JPD', 'JPP', 'CAD'}
+# set of instructions that use PC-relative addressing
+PC_REL_ADDR_INST = {'JPP'}
 
-# dictionary of binary equivalents of op codes
-OPCODE_BIN = {'NOP' : '00000', 'STP' : '00001', 'MVD' : '00010', 'MVS' : '00011', 'MVI' : '00100',
-              'MVB' : '00101', 'STR' : '00110', 'LOD' : '00111', 'NOT' : '01000', 'INC' : '01001',
-              'DCR' : '01010', 'ADA' : '01011', 'SBA' : '01100', 'XRA' : '01101', 'ANA' : '01110',
-              'ORA' : '01111', 'ADI' : '10000', 'SBI' : '10001', 'XRI' : '10010', 'ANI' : '10011',
-              'ORI' : '10100', 'JPD' : '10101', 'JPP' : '10110', 'JPR' : '10111', 'CAD' : '11000',
-              'CAR' : '11001', 'RET' : '11010', 'PSH' : '11011', 'POP' : '11100', 'RRC' : '11101',
-              'RLC' : '11110'}
+# ordered tuple of all flags
+ALL_FLAGS = ('U', 'Z', 'NZ', 'C', 'NC', 'P', 'M', 'OP')
 
-# dictionary of binary equivalents of flag names
-FLAG_BIN = {'U'  : '000', 'Z' : '001', 'NZ' : '010', 'C'  : '011',
-            'NC' : '100', 'P' : '101', 'M'  : '110', 'OP' : '111'}
+# # dictionary of binary equivalents of op codes
+# OPCODE_BIN = {'NOP' : '00000', 'STP' : '00001', 'MVD' : '00010', 'MVS' : '00011', 'MVI' : '00100',
+#               'MVB' : '00101', 'STR' : '00110', 'LOD' : '00111', 'NOT' : '01000', 'INC' : '01001',
+#               'DCR' : '01010', 'ADA' : '01011', 'SBA' : '01100', 'XRA' : '01101', 'ANA' : '01110',
+#               'ORA' : '01111', 'ADI' : '10000', 'SBI' : '10001', 'XRI' : '10010', 'ANI' : '10011',
+#               'ORI' : '10100', 'JPD' : '10101', 'JPP' : '10110', 'JPR' : '10111', 'CAD' : '11000',
+#               'CAR' : '11001', 'RET' : '11010', 'PSH' : '11011', 'POP' : '11100', 'RRC' : '11101',
+#               'RLC' : '11110'}
+
+# # dictionary of binary equivalents of flag names
+# ALL_FLAGS = {'U'  : '000', 'Z' : '001', 'NZ' : '010', 'C'  : '011',
+#             'NC' : '100', 'P' : '101', 'M'  : '110', 'OP' : '111'}
 
 # dictionary containing labels and corresponding line numbers
 label_line = {}
@@ -119,7 +130,7 @@ def main():
             if len(sys.argv) < 2:
                 print_error('no_arg_value', ['-pc'])
                 exit(1)
-            
+
             # make sure the supplied value is valid
             new_pc = int(sys.argv[1])
             if new_pc < 4 or new_pc > 65535:
@@ -128,13 +139,13 @@ def main():
 
             base_pc = new_pc
             del(sys.argv[:2])
-        
+
         elif sys.argv[0] == '-o':
             # make sure the argument isn't repeated
             if output_file != '':
                 print_error('repeated_arg', ['-o'])
                 exit(1)
-            
+
             # make sure a value is provided
             if len(sys.argv) < 2:
                 print_error('no_arg_value', ['-o'])
@@ -142,11 +153,11 @@ def main():
 
             output_file = sys.argv[1]
             del(sys.argv[:2])
-        
+
         else:
             print_error('invalid_arg', [sys.argv[0]])
             exit(1)
-    
+
     # set default values if not set by user
     if base_sp == -1:
         base_sp = DEFAULT_SP
@@ -154,9 +165,12 @@ def main():
         base_pc = DEFAULT_PC
     if not output_file:
         output_file = DEFAULT_OUTPUT_FILE
-    
+
     assemble(input_file, base_sp, base_pc, gen_coe, gen_hex, output_file)
-    #print(to_hex(16, 0, 117739894068434996696670576))
+    # print(to_hex(16, 0, 117739894068434996696670576))
+    # global label_line
+    # label_line['test'] = 50
+    # print(get_args(['ANI', '1', '0'], 40))
 
 def print_error(err_type, err_args):
     err_msg = ''
@@ -191,6 +205,10 @@ def print_error(err_type, err_args):
         err_msg = 'asm: internal error: invalid record type \'' + err_args[0] + \
                   '\' for I8HEX format.'
     
+    elif err_type == 'invalid_imm_value':
+        err_msg = 'asm: internal error: encountered immediate value in instruction \'' + \
+                  err_args[0] + '\', which does not use either immediate values or labels.'
+    
     print(err_msg)
 
 def assemble(input_file_name, base_sp, base_pc, gen_coe, gen_hex, output_file_name):
@@ -203,10 +221,10 @@ def assemble(input_file_name, base_sp, base_pc, gen_coe, gen_hex, output_file_na
 
     parsed_buffer = parse_input_file(input_file_name, base_pc)
     output_file_buffer = ''
-    
+
     # prepare bootstrap code
     bootstrap_code = [base_sp >> 8, base_sp & 0xff, base_pc >> 8, base_pc & 0xff]
-    
+
     # write bootstrap code
     pc_value = -1
     for line in bootstrap_code:
@@ -219,10 +237,8 @@ def assemble(input_file_name, base_sp, base_pc, gen_coe, gen_hex, output_file_na
 
         if gen_coe:  # add commas for coe files
             output_file_buffer += ','
-        
-        output_file_buffer += '\n'
 
-    print(output_file_buffer)
+        output_file_buffer += '\n'
 
     pc_value = DEFAULT_PC - 1
     nop_buffer_complete = base_pc == DEFAULT_PC  # true if the buffer NOPs between bootstrap and
@@ -240,38 +256,104 @@ def assemble(input_file_name, base_sp, base_pc, gen_coe, gen_hex, output_file_na
         if gen_coe and nop_buffer_complete:
             output_file_buffer += '; [' + str(pc_value) + '] ' + line + '\n'
 
-        # additional increase in line number for two-argument instructions
-        if is_imm_val_inst(line):
-            pc_value += 1
-
         # break the instruction into tokens
         tokens = [token.strip() for token in line.split()]
 
-        # initialize the binary instruction with the opcode
-        binary_inst = OPCODE_BIN[tokens[0]]
+        # compute the decimal equivalent of the instruction
+        line_dec = ALL_INST.index(tokens[0]) << 3
 
-        # assemble instructions with 0 arguments
+        # get the binary
+        line_bin = ''
+
         if len(tokens) == 1:
-            binary_inst += '000'
-            if gen_coe:  # add commas for coe files
-                binary_inst += ','
-            binary_inst += '\n'
-
-        # assemble instructions with atleast 1 argument
+            if gen_hex:
+                line_bin = to_hex(pc_value, 0, line_dec)
+            else:
+                line_bin = '{0:08b}'.format(line_dec)
         else:
-            args = get_bin_args(tokens, pc_value)
-            for arg in args:
-                binary_inst += arg
-                if gen_coe:  # add commas for coe files
-                    binary_inst += ','
-                binary_inst += '\n'
+            args = get_args(tokens, pc_value)
 
-        output_file_buffer += binary_inst
+            # write register/flag number
+            line_dec += args[0]
+            if gen_hex:
+                line_bin = to_hex(pc_value, 0, line_dec)
+            else:
+                line_bin = '{0:08b}'.format(line_dec)
+            
+            # write immediate value, if exists
+            if len(tokens) == 3:
+                pc_value += 1
+                if gen_hex:
+                    line_bin += '\n' + to_hex(pc_value, 0, args[1])
+                else:
+                    line_bin += '\n' + '{0:08b}'.format(args[1])
+        
+        output_file_buffer += line_bin + '\n'
 
+    # add end of file for Intel hex
+    if gen_hex:
+        output_file_buffer += to_hex(0, 1, 0) + '\n'
+    
     # write the buffer into output file
     output_file = open(output_file_name, 'w')
     output_file.write(output_file_buffer)
     output_file.close()
+
+def get_args(tokens, pc_value):
+    # extract and convert the first argument
+    args = [ALL_FLAGS.index(tokens[1]) if tokens[0] in FLAG_INST else int(tokens[1])]
+    
+    # extract and convert the second argument, if it exists
+    if len(tokens) == 3:
+        arg = 0
+        if is_num(tokens[2]):
+            arg = int(tokens[2])
+        else:
+            if tokens[0] in PC_REL_ADDR_INST:
+                # instructions that use PC-relative addresses
+                arg = label_line[tokens[2]] - pc_value - 2
+            elif tokens[0] in LABEL_ADDR_INST:
+                # instructions that use direct addresses
+                arg = label_line[tokens[2]]
+            else:
+                # fatal error
+                print_error('invalid_imm_value', [tokens[0]])
+                exit(1)
+                
+        args.append(arg % 256)
+    
+    return args
+
+def get_bin_args(tokens, pc_value):
+    '''
+    Extracts arguments from instruction tokens and returns their binary equivalent.
+    The first argument is converted and returned as-is, i.e. it is not padded to 8-bits.
+    The second argument is converted and sign extended to 8-bit length.
+    Requires len(tokens) >= 2.
+    '''
+    bin_args = []
+
+    # extract and convert the first argument
+    if tokens[0] in FLAG_INST:
+        bin_args += [ALL_FLAGS[tokens[1]]]
+    else:
+        bin_args += ['{0:03b}'.format(int(tokens[1]))]
+
+    # extract and convert the second argument, if it exists
+    if len(tokens) == 3:
+        arg = 0
+        if is_num(tokens[2]):
+            arg = int(tokens[2])
+        else:
+            if tokens[0] in LABEL_ADDR_INST:
+                # instructions that use direct addresses
+                arg = label_line[tokens[2]]
+            else:
+                # instructions that use PC-relative addresses
+                arg = label_line[tokens[2]] - pc_value
+        bin_args += ['{0:08b}'.format(arg % 256)]
+
+    return bin_args
 
 def to_hex(address, record_type, data):
     byte_count = 0
@@ -284,7 +366,7 @@ def to_hex(address, record_type, data):
                      '{0:04x}'.format(address) + ' ' + \
                      '00' + ' ' + \
                      hex(data)[2:].rjust(byte_count*2, '0') + ' '
-        
+
         # calculate checksum
         for val in [byte_count, address, data]:
             while val:
@@ -293,14 +375,14 @@ def to_hex(address, record_type, data):
             checksum &= 0xff
         checksum = '{0:02x}'.format((~checksum + 1) & 0xff)
         hex_value += checksum + ' '
-    
+
     elif record_type == 1:
         hex_value = ':00 0000 01 FF'
-    
+
     else:
         print_error('invalid_hex_record_type', [record_type])
         exit(1)
-    
+
     return hex_value
 
 def parse_input_file(filename, base_pc):
@@ -314,7 +396,7 @@ def parse_input_file(filename, base_pc):
     parsed_buffer = []
 
     # add NOP instructions between bootstrap code and user code
-    for i in range(4, base_pc):
+    for i in range(DEFAULT_PC, base_pc):
         parsed_buffer += ['NOP']
 
     # check user code for syntatical errors,
@@ -334,7 +416,7 @@ def parse_input_file(filename, base_pc):
         comment_index = line.find(';')
         if comment_index != -1:
             line = line[:comment_index]
-        
+
         if is_label(line):
             label_line[line[:-1]] = pc_value + 1
         elif is_valid_inst(line):
@@ -360,7 +442,7 @@ def init_regex():
 
     # prepare regex for all single byte instructions, except that cannot use flags
     INST_REGEX = r'\s*((('
-    for opcode in (set(OPCODE_BIN.keys()) - IMM_VAL_INST - FLAG_INST - {'STP'}):
+    for opcode in (set(ALL_INST) - IMM_VAL_INST - FLAG_INST - {'STP'}):
         INST_REGEX += r'(' + opcode + r')|'
     INST_REGEX = INST_REGEX[:-1] + r')\s+[0-7])|(STP)|(('
 
@@ -368,7 +450,7 @@ def init_regex():
     for opcode in (FLAG_INST - IMM_VAL_INST):
         INST_REGEX += r'(' + opcode + r')|'
     INST_REGEX = INST_REGEX[:-1] + r')\s+(('
-    for flag in FLAG_BIN.keys():
+    for flag in ALL_FLAGS:
         INST_REGEX += r'(' + flag + r')|'
     INST_REGEX = INST_REGEX[:-1] + r')|[0-7]))|(('
     
@@ -383,7 +465,7 @@ def init_regex():
     for opcode in LABEL_ADDR_INST:
         IMM_VAL_INST_REGEX += r'(' + opcode + r')|'
     IMM_VAL_INST_REGEX = IMM_VAL_INST_REGEX[:-1] + r')\s+(('
-    for flag in FLAG_BIN.keys():
+    for flag in ALL_FLAGS:
         IMM_VAL_INST_REGEX += r'(' + flag + r')|'
     IMM_VAL_INST_REGEX = IMM_VAL_INST_REGEX[:-1] + r')|[0-7])\s+((\d+)|([a-zA-Z]\w*))))\s*'
 
@@ -413,37 +495,6 @@ def is_imm_val_inst(line):
         return m.group() == line
     
     return False
-
-def get_bin_args(tokens, pc_value):
-    '''
-    Extracts arguments from instruction tokens and returns their binary equivalent.
-    The first argument is converted and returned as-is, i.e. it is not padded to 8-bits.
-    The second argument is converted and sign extended to 8-bit length.
-    Requires len(tokens) >= 2.
-    '''
-    bin_args = []
-
-    # extract and convert the first argument
-    if tokens[0] in FLAG_INST:
-        bin_args += [FLAG_BIN[tokens[1]]]
-    else:
-        bin_args += ['{0:03b}'.format(int(tokens[1]))]
-
-    # extract and convert the second argument, if it exists
-    if len(tokens) == 3:
-        arg = 0
-        if is_num(tokens[2]):
-            arg = int(tokens[2])
-        else:
-            if tokens[0] in LABEL_ADDR_INST:
-                # instructions that use direct addresses
-                arg = label_line[tokens[2]]
-            else:
-                # instructions that use PC-relative addresses
-                arg = label_line[tokens[2]] - pc_value
-        bin_args += ['{0:08b}'.format(arg % 256)]
-
-    return bin_args
 
 def is_num(num):
     '''
